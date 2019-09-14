db0564e47bae9b277e60e7f7ea6d6845250b99df
Victor <victor.du.77@hotmail.fr>
2019-09-14 11:56:51 +0200

    Course 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,7 @@
 // any CSS you require will output into a single css file (app.css in this case)
 import '../css/app.css';
 import getNiceMessage from './get_nice_message';
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+import $ from 'jquery';
+//global.$ = $;
 
 console.log(getNiceMessage(5));
